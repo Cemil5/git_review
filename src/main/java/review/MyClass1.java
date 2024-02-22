54cf0e1067be1cb56da7dea81c82f6d01688e61b
suleyman aydin <suleymanscience38@gmail.com>
2024-02-22 21:56:07 +0000

    updated suleyman's

--- src/main/java/review/MyClass1.java
+++ src/main/java/review/MyClass1.java
@@ -5,5 +5,6 @@ public class MyClass1 {
     public static void main(String[] args) {
         System.out.println("class1");
         System.out.println("efe");
+        System.out.println("kayseri");
     }
 }
